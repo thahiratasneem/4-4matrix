c0677a99d00adc02ee0fc4121f8634c2da861b49
thahiratasneem <sheik@Sheiks-MacBook-Pro.local>
2014-08-19 15:54:42 +0530

    4*4 matrix

---
+++ src/twodarray/Multabl.java
@@ -0,0 +1,21 @@
+package twodarray;
+
+public class Multabl {
+
+	public static  void main(String[] args) {
+		// TODO Auto-generated method stub
+int mvalue=1;
+int m,n;
+for(int i=1;i<=4;i++)
+{
+	for(int j=1;j<=4;j++)
+	{
+		System.out.print("\t " +mvalue );
+        mvalue++;
+	}
+	System.out.println("\n" );
+
+}
+
+	}
+}
